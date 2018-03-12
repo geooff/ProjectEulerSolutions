a2d948c856e666460b063acafae60182af20233e
Geoff <geoffbeamish@gmail.com>
2018-03-12 19:29:45 -0400

    PE_Q1

---
+++ PE_Q1.py
@@ -0,0 +1,16 @@
+#PE_Q1
+#Find the sum of all the multiples of 3 or 5 below 1000
+
+#make list of multiples of 3
+#make list of multiples of 5
+#sum lists
+total = 0
+listx = []
+for i in range(1000):
+	if i % 3 == 0 or i % 5 == 0:
+		listx.append(i)
+
+for i in listx:
+	total += i
+
+print total
