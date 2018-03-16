046106a0c4c40ec1b92c9dba04b4f8ff0ce8808a
Geoff <geoffbeamish@gmail.com>
2018-03-15 22:41:50 -0400

    Begin Work on Prime#

--- PE_Q3.py
+++ PE_Q3.py
@@ -9,12 +9,25 @@ orig_num = 600851475143
 num = 4000
 
 sqt_num = int(num ** 0.5)
+fact_list = []
+
 
 def get_factors(num):
-	fact_list = []
 	for i in range(1, num):
 		if num % i == 0:
 			fact_list.append(i)
 	return fact_list
 
 print get_factors(num)
+
+def get_prime_factores(factors):
+	prime_fact_list = []
+	for i in range(len(factors)):
+		print fact_list(i)		
+		#for j in range(get_factors(i)):
+		#	if get_factors(i) % j == 0:
+		#		prime_fact_list.append(get_factors(i))
+	#return prime_fact_list
+
+print get_prime_factores(get_factors(num))
+				
