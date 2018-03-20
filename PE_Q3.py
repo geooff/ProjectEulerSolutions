f58befbf957f01b48a4de6cc40019da9b67c7966
Geoff <geoffbeamish@gmail.com>
2018-03-20 17:45:44 -0400

    3.1

--- PE_Q3.py
+++ PE_Q3.py
@@ -12,7 +12,7 @@ fact_list = []
 prime_list = []
 
 def get_factors(num): #Takes a large number and returns factors
-	for i in xrange(1, num):
+	for i in xrange(num/2, 1, -1):
 		if num % i == 0:
 			fact_list.append(i)
 			#break
@@ -31,17 +31,24 @@ def get_prime(num): #Takes a list of factors and returns their primality
 		prime_list.append(is_prime)
 	return prime_list
 
-#print get_factors(num)
-print get_prime(get_factors(num))
+def is_prime(num):
+	is_prime = False
+	for i in xrange(2, num):
+		if num % i == 0:
+			is_prime = False
+			break
+		else:
+			is_prime = True
+	return is_prime
+
+def max_prime_fact(num):
+	for i in xrange(num/2, 1, -1):
+		if num % i == 0:
+			if is_prime(i) == True:
+				return i
+				break
+
+print max_prime_fact(num)
 #print get_prime(get_factors(num))
 
-# def get_prime(num):
-# 	is_prime = False
-# 	for i in xrange(2, num):
-# 		if num % i == 0:
-# 			is_prime = False
-# 			break
-# 		else:
-# 			is_prime = True
-# 	return is_prime
 				
