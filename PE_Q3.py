7d11d35864421aba1f6f6060258797589f4f11b0
Geoff <geoffbeamish@gmail.com>
2018-03-20 19:58:13 -0400

    Sol 3.2

--- PE_Q3.py
+++ PE_Q3.py
@@ -2,53 +2,27 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
-#ToDo
-#How to find if prime?
-
 orig_num = 600851475143
-num = 600851475143
+a = 600851475143
+b = 2
+c = 0
 
-fact_list = []
-prime_list = []
+def get_prime_factor(a, b, c): #Takes a large number and returns factors
+	print "Start"
+	print "The current Largest devisor is: ",  c
+	print "The current number we are trying to factor is: ", a
+	while b < a/2:
+		if a % b != 0:
+			b += 1
+		elif a % b == 0:
+			c = b
+			a = a / b
+			if b > c:
+				c = b
+			b = 2
+			print "The current Largest devisor is: ",  c
+			print "The current number we are trying to factor is: ", a
+	print a
+	return "Finished!"
 
-def get_factors(num): #Takes a large number and returns factors
-	for i in xrange(num/2, 1, -1):
-		if num % i == 0:
-			fact_list.append(i)
-			#break
-	return fact_list
-
-def get_prime(num): #Takes a list of factors and returns their primality
-	is_prime = False
-	for j in num:
-		prime_list.append(j)
-		for i in xrange(2, j):
-			if j % i == 0:
-				is_prime = False
-				break
-			else:
-				is_prime = True
-		prime_list.append(is_prime)
-	return prime_list
-
-def is_prime(num):
-	is_prime = False
-	for i in xrange(2, num):
-		if num % i == 0:
-			is_prime = False
-			break
-		else:
-			is_prime = True
-	return is_prime
-
-def max_prime_fact(num):
-	for i in xrange(num/2, 1, -1):
-		if num % i == 0:
-			if is_prime(i) == True:
-				return i
-				break
-
-print max_prime_fact(num)
-#print get_prime(get_factors(num))
-
-				
+print get_prime_factor(a, b, c)
