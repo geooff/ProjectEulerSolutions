46069c610c3fb3a2c9a9fb3923b89e3c706a6b76
Geoff <geoffbeamish@gmail.com>
2020-02-15 00:01:38 -0500

    init PE_Q18.py solution
    Signed-off-by: geoff <geoffbeamish@gmail.com>

---
+++ PE_Q18.py
@@ -0,0 +1,115 @@
+"""
+By starting at the top of the triangle below and moving to adjacent numbers on the row below,
+the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
+However, Problem 67, is the same challenge with a triangle containing one-hundred rows;
+it cannot be solved by brute force, and requires a clever method! ;o)
+"""
+triangle = [
+    [75],
+    [95, 64],
+    [17, 47, 82],
+    [18, 35, 87, 10],
+    [20, 4, 82, 47, 65],
+    [19, 1, 23, 75, 3, 34],
+    [88, 2, 77, 73, 7, 63, 67],
+    [99, 65, 4, 28, 6, 16, 70, 92],
+    [41, 41, 26, 56, 83, 40, 80, 70, 33],
+    [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+    [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+    [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+    [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+    [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+    [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
+]
+
+
+def clever(triangle_list):
+    def get_path(triangle_list):
+        output = list()
+        def decend_tree(tree, res):
+            if len(tree) == 1:
+                if tree[0][0]:
+                    output.append(res + tree[0][0])
+                if tree[0][1]:
+                    output.append(res + tree[0][1])
+            else:
+                if tree[0][0]:
+                    left = tree[1:]
+                    decend_tree(left, tree[0][0] + res)
+                if tree[0][1]:
+                    right = [row[1:] for row in tree[1:]]
+                    decend_tree(right, tree[0][1] + res)
+        decend_tree(triangle_list[1:], triangle_list[0][0])
+        return output
+
+    i = 0
+    while len(get_path(triangle_list)) != 0:
+        for row_idx, row in enumerate(triangle_list):
+            for col_idx, ele in enumerate(row):
+                if ele == i:
+                    triangle_list[row_idx][col_idx] = None
+        i += 1
+
+
+def naive(triangle_list):
+    output = []
+    def decend_tree(tree, res):
+        if len(tree) == 1:
+            output.append(res + tree[0][0])
+            output.append(res + tree[0][1])
+        else:
+            left = tree[1:]
+            right = [row[1:] for row in tree[1:]]
+
+            decend_tree(left, tree[0][0] + res)
+            decend_tree(right, tree[0][1] + res)
+
+    decend_tree(triangle_list[1:], triangle_list[0][0])
+    return output
+
+def test():
+    def _is_triangle(triangle_list):
+        last_row = 0
+        for i in triangle_list:
+            if len(i) < last_row:
+                return False
+            last_row = len(i)
+        return True
+
+    assert _is_triangle([[1],[1,2],[1,2,3]])
+    assert _is_triangle(triangle)
+    assert max(naive(triangle)) == 1074
+
+
+test()
+angry = clever(triangle)
+print(len(angry))
+angry.sort(reverse = True)
+print(angry)
